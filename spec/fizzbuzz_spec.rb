b281c9e0f837f402e699d7045026e631a7613f61
bcarrol2 <blairacarroll@gmail.com>
2018-09-29 20:02:04 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,7 +5,7 @@ require_relative '../fizzbuzz.rb'
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
-end
+
     expect(fizz_3).to eq("Fizz")
   end
   it 'returns "Buzz" when the number is divisible by 5' do
@@ -23,4 +23,4 @@ end
 
     expect(fizz_4).to eq(nil)
   end
-
+end
