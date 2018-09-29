902eac1f080e48a52f36338be5fba1a421384595
bcarrol2 <blairacarroll@gmail.com>
2018-09-29 19:22:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,2 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-
-fizz_3 = fizzbuzz(3)
-if fizz_3 / 3
-puts TRUE
-else FALSE
-end
