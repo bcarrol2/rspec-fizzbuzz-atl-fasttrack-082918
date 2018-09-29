12274d0fb548d44eecf8e248508645088fd142c6
bcarrol2 <blairacarroll@gmail.com>
2018-09-29 19:34:48 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,16 +1,10 @@
 require_relative './spec_helper.rb'
-
+require_relative '../fizzbuzz.rb'
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
-    def fizzbuzz
-      
-    end
-    fizz_3 = "Fizz"
-if fizzbuzz 
-puts TRUE
-else FALSE
+    fizz_3 = fizzbuzz(3)
 end
     expect(fizz_3).to eq("Fizz")
   end
